801cb170faed8ce828874a2d62dca0f62369ad87
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-07-29 14:53:52 +0300

    feat: implement OnNavigationSelectedListener

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.recordkeeper
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
 import com.example.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,6 +21,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textViewExample.text = "Testing one 2"
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(this)
+    }
+
+    private fun onRunningClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+    }
+
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_cycling) {
+            onCyclingClicked()
+            return true
+        } else if (item.itemId == R.id.nav_running) {
+            onRunningClicked()
+            return true
+        } else {
+            return false
+        }
     }
 }
